34f742c77fee9e6b33d40e854d33790c4c58fa51
Aguerrerosipac <githubalicia@gmail.com>
2022-03-07 16:21:04 -0600

    Insertando filas

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello");
+ 
+const button= document.querySelector("button");
+   button.onclick = function agregarFila(){
+    document.getElementById("tabla").insertRow(-1).innerHTML = '<td>New cell1</td> <td>New cell2</td>';    
+       
+  };
+
+  
